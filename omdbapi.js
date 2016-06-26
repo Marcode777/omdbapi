00714e9a1330721ffd7cdb2fbfce26737cd8d500
Marcode777 <marcosebello7@gmail.com>
2016-06-26 12:28:33 -0400

    input from user in submit button is now getting its value saved and will be concatenated to the http verb url to retrieve the JSON for that movie

--- omdbapi.js
+++ omdbapi.js
@@ -1,19 +1,25 @@
-$("#target").submit(function(){
+$("#target").submit(function( e ){
+    e.preventDefault();
+    var yeah = $("input[type=text]").val();
     $.ajax({
-       type:"POST",
-        url:"http://www.omdbapi.com/?" + yeah
+        type:"POST",
+        url: "http://www.omdbapi.com/?s=" + yeah + "&type=movie",
         success: function(data, textStatus, jqXHR) {
-        console.log(data);
-        var yeah = data;
-
-        document.getElementById("here").innerHTML = ;
-      },
+            // console.log(data);
+            for ( i = 0; i < data.Search.length; i++ ) {
+                console.log(data.Search[i]);
+                //instead of using inner.HTML to display on html page, use jQuery syntax .append
+                $("<p>").text(data.Search[i].Title).appendTo( $(".here") );
+                $("<p>").text(data.Search[i].Year).appendTo( $(".here") );
+            }
+        },
   
-    error: function(jqXHR, textStatus, errorThrown){
-     console.log(jqXHR);
-     console.log(textStatus);
-     console.log(errorThrown);
-    })
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
 });
 
 
